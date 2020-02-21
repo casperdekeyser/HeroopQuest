2633265fce6d2841de1fd6c00b2c50ff4bcfad3b
Casper De Keyser <casper.dekeyser@student.ap.be>
2020-02-21 11:31:14 +0100

    Heldklasse aangepast

--- src_HeroopQuest/src_HeroopQuest/GameClasses/Held.cs
+++ src_HeroopQuest/src_HeroopQuest/GameClasses/Held.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace src_HeroopQuest.GameClasses
 {
+    enum HeroType {Barbaar, Dwerg, Elf, Tovenaar}
     class Held
     {
+        public string Name { get;  set; }
+        public string Description { get; private set; }
+        public int MaxIntelligence { get; private set; }
+        public int CurrentIntelligence { get; set; }
+        public int MaxBody { get; private set; }
+        public int CurrentBody { get; set; }
+        public HeroType HeroType { get; private set; }
+
+        public int AttackValue { get; private set; } = 2;
+        public int DefenseVallue { get; private set; } = 2;
+        public int RunValue { get; private set; } = 2;
     }
 }
